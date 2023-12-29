f70554423eb33bc86888c9c1f762685642ef694c
icerbaby <145264735+icerbaby@users.noreply.github.com>
2023-12-29 15:27:56 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import *
+from ui import Ui_MainWindow
+
+class Widget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+app = QApplication([])
+ex = Widget()
+
+ex.show()
+app.exec()
